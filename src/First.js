d9c20bc0c0fd71e55c54b5691335ef9043cfd825
Aarjav Chauhan <aarjavchauhan439@gmail.com>
2022-03-21 16:11:31 -0400

    added tabler react to package json &  first js

--- src/First.js
+++ src/First.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { Outlet, Link } from "react-router-dom";
 
+import "tabler-react/dist/Tabler.css";
+import { Card, Button } from "tabler-react";
+
+
 function First (props) {
 
     return (
