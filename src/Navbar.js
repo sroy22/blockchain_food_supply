4fadcf0b0dafaadb512b303b5ec3382444a84ba9
Aarjav Chauhan <aarjavchauhan439@gmail.com>
2022-03-25 15:17:38 -0400

    Navbar fixes

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,7 +7,18 @@ class Navbar extends Component {
     return (
 
 <Nav>
-  <Nav.Item hasSubNav value="CSC 2512 blockchain_food_supply" icon="globe" to="https://github.com/sroy22/blockchain_food_supply">
+ <Nav.Item value="CSC 2512 Blockchain Food Supply" id="account" to="https://github.com/sroy22/blockchain_food_supply"/>
+  <Nav.Item hasSubNav value="Menu" icon="menu">
+    <Nav.SubItem value="Farmer" to="/farmer" />
+    <Nav.SubItem value="Processor" to="/processor" />
+    <Nav.SubItem value="Investor" to="/investor" />
+    {/* <Nav.SubItem value="Insurance" to={{
+            pathname: "/insurance", 
+            state: {
+                customers: []
+                }
+            }}/> */}
+    <Nav.SubItem value="Customer" to="/customer" />
   </Nav.Item>
   <Nav.Item active icon="user" id="account">
    {this.props.account}
